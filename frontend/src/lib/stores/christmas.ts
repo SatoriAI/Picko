a0de6e64cabd3c03542794800237d3a7aa122e09
Dawid Hanrahan <dawidhanrahan@gmail.com>
2025-12-16 23:04:49 +0100

    Fix handling christmas theme variable

--- frontend/src/lib/stores/christmas.ts
+++ frontend/src/lib/stores/christmas.ts
@@ -1,8 +1,8 @@
-import { PUBLIC_CHRISTMAS_THEME } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
 /**
  * Christmas theme toggle.
  * Controlled via PUBLIC_CHRISTMAS_THEME environment variable.
  * Set to "true" to enable festive decorations.
  */
-export const CHRISTMAS_THEME = PUBLIC_CHRISTMAS_THEME === 'true';
+export const CHRISTMAS_THEME = env.PUBLIC_CHRISTMAS_THEME === 'true';
